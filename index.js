83430cd219c40f362e4d91f48adb6799afa0f811
tui <70031851+tuisapo@users.noreply.github.com>
2023-02-02 18:23:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv/config')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+})
+
+client.on('ready', () => {
+    console.log('bot ta pronto')
+})
+
+
+client.on('messageCreate', message =>{
+    if(message.content === 'ping'){
+        message.reply('pong')
+    }
+})
+
+client.login(process.env.TOKEN)
